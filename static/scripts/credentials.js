a28c4c5c73f46643eba63b83a72833c3d2c34336
dhruv <dhruvpopat2912@gmail.com>
2020-06-22 16:03:43 -0700

    updated Credentials

--- static/scripts/credentials.js
+++ static/scripts/credentials.js
@@ -1,21 +1,21 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDicE8wQSGD8TGaj2RQtVRJJfIwtQNn6eU",
-    authDomain: "watchful-muse-276102.firebaseapp.com",
-    databaseURL: "https://watchful-muse-276102.firebaseio.com",
-    projectId: "watchful-muse-276102",
-    storageBucket: "watchful-muse-276102.appspot.com",
-    messagingSenderId: "270033416605",
-    appId: "1:270033416605:web:22963fdf6e1d3ec9503f74",
-    measurementId: "G-C6B693S10E"
-  };
+  apiKey: "Enter your API KEY",
+  authDomain: "watchful-muse-276102.firebaseapp.com",
+  databaseURL: "https://watchful-muse-276102.firebaseio.com",
+  projectId: "watchful-muse-276102",
+  storageBucket: "watchful-muse-276102.appspot.com",
+  messagingSenderId: "270033416605",
+  appId: "1:270033416605:web:22963fdf6e1d3ec9503f74",
+  measurementId: "G-C6B693S10E",
+};
 
-    // Initialize Firebase and Firestore
-    firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
+// Initialize Firebase and Firestore
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-    const authentication = firebase.auth();
-    const database = firebase.firestore();
+const authentication = firebase.auth();
+const database = firebase.firestore();
 
-    //google api key
-    const apiKey = 'AIzaSyDicE8wQSGD8TGaj2RQtVRJJfIwtQNn6eU';
+//google api key
+const apiKey = "Enter your API KEY";
